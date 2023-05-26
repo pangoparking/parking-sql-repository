799e73f9695502c1ea2690d192ca66b01412266a
volnykot <volnykot3@gmail.com>
2023-05-26 13:59:08 +0300

    adds repos for preporatory

--- src/main/java/org/parking/repo/CarsRepository.java
+++ src/main/java/org/parking/repo/CarsRepository.java
@@ -2,7 +2,14 @@ package org.parking.repo;
 
 import org.parking.entities.CarEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CarsRepository extends JpaRepository<CarEntity, Long> {
 
+	@Query(value = """
+			SELECT *
+			FROM cars
+			WHERE cars.car_number = :parkingID
+						""", nativeQuery = true)
+	CarEntity getCarInfo(long parkingID);
 }
--- src/main/java/org/parking/repo/ParkingLotsRepository.java
+++ src/main/java/org/parking/repo/ParkingLotsRepository.java
@@ -2,7 +2,16 @@ package org.parking.repo;
 
 import org.parking.entities.ParkingLotEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ParkingLotsRepository extends JpaRepository<ParkingLotEntity, Long> {
+	
+	@Query(value = """
+			SELECT *
+			FROM areas
+			JOIN parking_lots ON areas.areaid = parking_lots.areaid
+			WHERE parking_lots.parking_lot_id = :parkingID
+			""", nativeQuery = true)
+	byte [] getParkingLotInfo(long parkingID);
 
 }
